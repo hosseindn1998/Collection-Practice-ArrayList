7889ea50c5cc3ae0350a23f389c20fdf9fec3724
hosseindn1998 <hosseindn1998@gmail.com>
2024-02-15 19:57:44 +0330

    Innitialize and get number for Array list and add them

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        ArrayList<Integer> integers = new ArrayList<>();
+        ArrayList<Integer> goodIntegers = new ArrayList<>();
+        int flag=0;
+        int count=0;
+        int temp;
+        while (flag==0){
+            System.out.println("Enter Numbers : (End with -1)");
+            temp=scanner.nextInt();
+            scanner.nextLine();
+            if(temp==-1){
+                System.out.println("Are you Sure Ending? Please Enter 0 Else Input Your Number");
+                temp=scanner.nextInt();
+                scanner.nextLine();
+                if(temp==0)break;
+            }
+            integers.add(count,temp);
+            count++;
 
+
+        }
+        System.out.println("User Input Integers : \n"+integers);
+
+    }
 }
